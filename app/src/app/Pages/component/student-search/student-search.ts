0406462867798d44604046ab02018ba6dd4b2a45
khosatho <monokhosatho@gmail.com>
2025-07-08 15:02:15 +0200

    added components

---
+++ app/src/app/Pages/component/student-search/student-search.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-search',
+  imports: [],
+  templateUrl: './student-search.html',
+  styleUrl: './student-search.css'
+})
+export class StudentSearch {
+
+}
---
+++ app/src/app/Pages/component/student-update/student-update.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-update',
+  imports: [],
+  templateUrl: './student-update.html',
+  styleUrl: './student-update.css'
+})
+export class StudentUpdate {
+
+}
