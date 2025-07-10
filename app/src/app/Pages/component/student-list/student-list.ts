db288aed48967aa89e207516d12c792dbe852235
Earthchild2 <nthabiseng.fakude2@gmail.com>
2025-07-10 10:05:04 +0200

    added html for student list

--- app/src/app/Pages/component/student-list/student-list.ts
+++ app/src/app/Pages/component/student-list/student-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-student-list',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './student-list.html',
   styleUrl: './student-list.css'
 })
-export class StudentList {
+export class StudentListComponent implements OnInit {
+  
+  
+  students: Student[] = [];
 
+  constructor(private studentService: ) {}
+
+  ngOnInit(): void {
+    this.studentService.getStudents().subscribe((data: Student[]) => {
+      this.students = data;
+    });
+  }
 }
+
+
