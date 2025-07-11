f6e261c7cb726b194ff0ee7701a89aba5a2a8c12
khosatho <monokhosatho@gmail.com>
2025-07-11 09:45:07 +0200

    fixed routes

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,20 +1,12 @@
 import { Routes } from '@angular/router';
 import { StudentCreate } from './Pages/component/student-create/student-create';
-<<<<<<< HEAD
-import { StudentList } from './Pages/component/student-list/student-list';
+import { StudentListComponent } from './Pages/component/student-list/student-list';
 import { StudentSearch } from './Pages/component/student-search/student-search';
 import { StudentUpdate } from './Pages/component/student-update/student-update';
 
 export const routes: Routes = [
-    {path:'', component: StudentList },
+    {path:'', component: StudentListComponent },
     {path:'Add', component: StudentCreate },
     {path: 'Search', component: StudentSearch },
     {path: 'Update', component: StudentUpdate }
-=======
-import { StudentListComponent } from './Pages/component/student-list/student-list';
-
-export const routes: Routes = [
-    {path:'', component: StudentCreate },
-    {path:'list',component: StudentListComponent}
->>>>>>> 6d2efd6efa9a68c533bd3cf017d11390442cf4fe
-];
+]
