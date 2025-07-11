9049c7b712f6d4dc3966b57fc7f5ec2815aec1fb
khosatho <monokhosatho@gmail.com>
2025-07-11 08:58:03 +0200

    added routes

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { StudentCreate } from './Pages/component/student-create/student-create';
+import { StudentList } from './Pages/component/student-list/student-list';
+import { StudentSearch } from './Pages/component/student-search/student-search';
+import { StudentUpdate } from './Pages/component/student-update/student-update';
 
 export const routes: Routes = [
-    {path:'', component: StudentCreate }
+    {path:'', component: StudentList },
+    {path:'Add', component: StudentCreate },
+    {path: 'Search', component: StudentSearch },
+    {path: 'Update', component: StudentUpdate }
 ];
